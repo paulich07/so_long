54bda382efa85a279eb4e4db8523d9f3ba742700
Paulina Lichota <plichota@c1r5p10.42firenze.it>
2025-03-12 16:33:22 +0100

    minilibx funziona

--- include/so_long.h
+++ include/so_long.h
@@ -6,12 +6,15 @@
 /*   By: plichota <plichota@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 12:34:58 by plichota          #+#    #+#             */
-/*   Updated: 2025/03/12 16:05:57 by plichota         ###   ########.fr       */
+/*   Updated: 2025/03/12 16:16:57 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
-#   define SO_LONG_H
+# define SO_LONG_H
+
+#include "libft.h"
+#include "mlx.h"
 
 #include <unistd.h>
 #include <stdlib.h>
--- src/main.c
+++ src/main.c
@@ -6,11 +6,31 @@
 /*   By: plichota <plichota@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 12:34:24 by plichota          #+#    #+#             */
-/*   Updated: 2025/03/12 15:23:03 by plichota         ###   ########.fr       */
+/*   Updated: 2025/03/12 16:30:06 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "so_long.h"
+
 int main(void)
 {
+    printf("is alpha: %d", ft_isalpha(97));
+
+    void *mlx;
+    void *win;
+
+    // Inizializza la libreria mlx
+    mlx = mlx_init();
+    
+    // Crea una finestra con dimensioni 800x600 e un titolo
+    win = mlx_new_window(mlx, 800, 600, "Finestra Minilibx");
+
+    // Colore di sfondo (rosso in RGB)
+    mlx_clear_window(mlx, win);
+    mlx_string_put(mlx, win, 10, 10, 0xFFFFFF, "Ciao Minilibx!");
+
+    // Mantieni la finestra aperta fino a che non viene chiusa
+    mlx_loop(mlx);
+
     return (0);
 }
